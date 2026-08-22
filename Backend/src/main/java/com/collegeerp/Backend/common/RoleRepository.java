0a6fbbf0d3957a32fb908d322027a410d93a6df8
Gummadala vikas <vikas93912@gmail.com>
2026-08-22 19:28:52 +0530

    Harden role name lookup against case duplicates

--- Backend/src/main/java/com/collegeerp/Backend/common/RoleRepository.java
+++ Backend/src/main/java/com/collegeerp/Backend/common/RoleRepository.java
@@ -8,6 +8,10 @@ public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Optional<Role> findByName(String name);
 
+    Optional<Role> findByNameIgnoreCase(String name);
+
     boolean existsByName(String name);
+
+    boolean existsByNameIgnoreCase(String name);
 
 }
