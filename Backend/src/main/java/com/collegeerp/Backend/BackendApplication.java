cc3e90cf9ca96c02cbd6b5a277146c6d946f7d97
Gummadala vikas <vikas93912@gmail.com>
2026-08-23 22:00:47 +0530

    Remove database credential logging from application startup

--- Backend/src/main/java/com/collegeerp/Backend/BackendApplication.java
+++ Backend/src/main/java/com/collegeerp/Backend/BackendApplication.java
@@ -1,10 +1,7 @@
 package com.collegeerp.Backend;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -13,22 +10,5 @@ public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner checkEnv(Environment env) {
-        return args -> {
-            System.out.println("=================================");
-            System.out.println("DB_URL      = " + env.getProperty("DB_URL"));
-            System.out.println("DB_USERNAME = " + env.getProperty("DB_USERNAME"));
-            System.out.println("=================================");
-        };
-    }
-    @Bean
-    CommandLineRunner printDb(Environment env) {
-        return args -> {
-            System.out.println("DB_URL = " + env.getProperty("DB_URL"));
-            System.out.println("DB_USERNAME = " + env.getProperty("DB_USERNAME"));
-        };
     }
 }
