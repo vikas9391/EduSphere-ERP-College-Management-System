39fff039d7dcc1d5498dc1fd67648a40fd84cdcc
Gummadala vikas <vikas93912@gmail.com>
2026-09-16 15:13:14 +0530

    chore: export mobile screens

---
+++ app/src/screens/index.ts
@@ -0,0 +1,6 @@
+export { DashboardScreen } from './DashboardScreen';
+export { ClassesScreen } from './ClassesScreen';
+export { AttendanceScreen } from './AttendanceScreen';
+export { AssignmentsScreen } from './AssignmentsScreen';
+export { TimetableScreen } from './TimetableScreen';
+export { ProfileScreen } from './ProfileScreen';
