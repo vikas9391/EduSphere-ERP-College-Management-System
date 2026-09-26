2b3191c240fc2930a4fa83dc3b43e17e068ee168
Gummadala vikas <vikas93912@gmail.com>
2026-09-26 13:39:47 +0530

    Add fee management FeeStructureRepository.java

---
+++ Backend/src/main/java/com/collegeerp/Backend/fee/repository/FeeStructureRepository.java
@@ -0,0 +1,4 @@
+package com.collegeerp.Backend.fee.repository;
+import com.collegeerp.Backend.fee.entity.FeeStructure;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface FeeStructureRepository extends JpaRepository<FeeStructure, Long> {}
